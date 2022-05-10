09f2028fc4e6c1424f9a49b567c2b403d859aad5
Gede <gedewir@outlook.com>
2022-05-10 17:44:20 +0800

    start account manager

--- js/containers.js
+++ js/containers.js
@@ -1,10 +1,11 @@
 function createBox(name, url, image) {
 var div = document.createElement('div');
-div.classList.add('col-xl');
+div.classList.add('col-sm');
 div.classList.add('text-center');
 div.innerHTML = '<a href="'+url+'">'+ '<img src="'+image+'" id="box-images" />' + '</br>' + name + '</a>' ;
 var box = document.getElementById('box');
 box.appendChild(div);
+console.log(div);
                         }
 
 var dataset;
@@ -16,4 +17,5 @@ d3.csv("admin.csv", function(data) {
 
 function print1() {
   createBox(dataset.website, dataset.url, dataset.image);
+
                   };
